d0c804de03f4adbd529edbab9902748239e4eed2
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-16 21:25:34 +0800

    Add api_client.py to retrive videos from youtube

---
+++ src/api_client.py
@@ -0,0 +1,74 @@
+import os
+import utils
+from googleapiclient.discovery import build
+
+def get_youtube_service():
+    SECRET_API_KEY = os.environ.get("SECRET_API_KEY")
+    youtube = build("youtube", "v3", developerKey=SECRET_API_KEY)
+    return youtube
+
+
+def search_videos(youtube, playlist_id, videos):
+
+    nextPageToken = None
+
+    while True:
+        playlist_request = youtube.playlistItems().list(
+            part="contentDetails",
+            playlistId=playlist_id,
+            maxResults=50,
+            pageToken=nextPageToken
+        )
+
+        playlist_response = playlist_request.execute()
+
+        video_ids = []
+        for item in playlist_response["items"]:
+            video_ids.append(item["contentDetails"]["videoId"])
+
+        video_request = youtube.videos().list(
+            part = "statistics,snippet",
+            id = ",".join(video_ids)
+        )
+
+        video_response = video_request.execute()
+
+
+        for item in video_response["items"]:
+            video_title = item["snippet"]["title"]
+
+            video_id = item["id"]
+            episode_number = utils.get_episode_number(video_title)
+            youtube_link = f"https://youtu.be/{video_id}"
+
+            videos.append(
+                    {
+                        "title" : video_title,
+                        "episode_number" : episode_number,
+                        "url" : youtube_link
+                    }
+            )
+
+        nextPageToken = playlist_response.get("nextPageToken")
+
+        if not nextPageToken:
+            return videos
+
+
+def main():
+    videos = []
+    playlist_id = "PL5UEpsh7xfCIMBsh7viJcd3HBjEqJt-Do"
+
+    youtube = get_youtube_service()
+    videos = search_videos(youtube, playlist_id, videos)
+    videos.sort(key=lambda vid:vid["episode_number"])
+
+    for video in videos:
+        print(f"Title: {video['title']}\Episode Number: {video['episode_number']}\nLink: {video['url']}")
+        print()
+
+
+    print(len(videos))
+
+if __name__ == "__main__":
+   main() 
