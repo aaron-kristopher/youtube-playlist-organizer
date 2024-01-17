7ee369238acc657baaa263cccec76863b7a5b6fa
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-17 23:15:56 +0800

    Create function to check missing episodes

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import re
 import csv
+from itertools import islice
 
 
 regex = r"(?i)(?:episode|ep)\s*(?P<episode_number>\d{1,4})(?:\s+of\s+\d+\s*)?.*"
@@ -51,11 +52,13 @@ def get_video_id_from_csv(videos_csv):
         print("Error: Column video id not found in the CSV file.")
         return None
 
+
 def get_videos_from_csv(videos_csv):
-    with open(videos_csv) as f:
-        reader = csv.DictReader(f)
+    with open(videos_csv) as csvfile:
+        reader = csv.DictReader(csvfile)
         videos = list(reader)
     return videos
+
 
 def remove_duplicate_videos(videos):
     seen = set()
@@ -68,6 +71,7 @@ def remove_duplicate_videos(videos):
             new_videos.append(video)
     return new_videos
 
+
 def save_to_csv(videos):
     field_names = list(videos[0].keys())
 
@@ -75,6 +79,7 @@ def save_to_csv(videos):
         writer = csv.DictWriter(csvfile, fieldnames = field_names) 
         writer.writeheader() 
         writer.writerows(videos)
+
 
 def check_missing_videos(videos):
     missing_episodes = []
@@ -97,11 +102,35 @@ def check_missing_videos(videos):
     return missing_episodes
 
 
+def get_remaining_videos(videos_csv, last_video):
+    last_video = offset_missing_episodes(last_video)
+
+    with open(videos_csv) as csvfile:
+        reader = csv.reader(csvfile)
+        rows = list(reader)  
+        headers = rows[0]  
+        data = rows[last_video:]  
+        videos = [dict(zip(headers, row)) for row in data[1:]]  # Convert remaining rows to dictionaries
+
+    print(videos[0]["episode_number"])
+    return videos
+
+
+def offset_missing_episodes(last_video):
+    missing_episodes = [136, 305, 306, 307, 308, 449]
+    
+    for index, episode in enumerate(missing_episodes):
+        if last_video < episode:
+            return last_video - (index +  1)
+
+    return last_video
+
+
 if __name__ == "__main__":
-    #video_id = get_video_id_from_csv("./videos.csv")
     videos = get_videos_from_csv("./videos.csv")
     missing_episodes = check_missing_videos(videos)
 
+    vidoes = get_remaining_videos("./videos.csv", 350)
     print(f"Missing episodes: {missing_episodes}")
 
 
