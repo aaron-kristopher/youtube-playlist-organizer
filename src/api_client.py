23cf17a17c2d42af0c78c8c6df450b92b9f34b28
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-17 10:19:24 +0800

    Transfer non api related functions to util.py for code consistency

--- src/api_client.py
+++ src/api_client.py
@@ -42,15 +42,14 @@ def search_videos_from_playlist(playlist_id):
             video_id = item["id"]
             youtube_link = f"https://youtu.be/{video_id}"
 
-            if episode_number is not None:
-                videos.append(
-                            {
-                            "title" : video_title,
-                            "episode_number" : episode_number,
-                            "video_id" : video_id,
-                            "url" : youtube_link
-                        }
-                )
+            videos.append(
+                        {
+                        "title" : video_title,
+                        "episode_number" : episode_number,
+                        "video_id" : video_id,
+                        "url" : youtube_link
+                    }
+            )
 
         nextPageToken = playlist_response.get("nextPageToken")
 
@@ -58,27 +57,6 @@ def search_videos_from_playlist(playlist_id):
             return videos
 
 
-def get_sorted_videos(playlist_id):
-        videos = search_videos_from_playlist(playlist_id)
-
-        sort_videos(videos)
-
-        for video in videos:
-            print(f"Title: {video['title']}\nEpisode Number: {video['episode_number']}\nLink: {video['url']}")
-            print()
-
-        print(f"Videos Retrieved: {len(videos)}")
-
-        return videos
-
-
-
-
-def sort_videos(videos):
-    videos.sort(key=lambda vid:vid["episode_number"])
-
-
-
 if __name__ == "__main__":
     playlist_id = "PL5UEpsh7xfCIMBsh7viJcd3HBjEqJt-Do"
-    get_sorted_videos(playlist_id)
+    search_videos_from_playlist(playlist_id)
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import re
+from src.api_client.py import search_videos_from_playlist
+
 
 regex = r"(?i)(?:episode|ep)\s*(?P<episode_number>\d{1,4})(?:\s+of\s+\d+\s*)?.*"
+
 
 def get_episode_number(title):
     match = re.search(regex, title)
@@ -9,3 +12,30 @@ def get_episode_number(title):
     else:
         return None  # Or handle cases where no episode number is found
 
+
+def remove_non_episode_video(videos):
+
+    for index, video in enumerate(videos):
+        if video["episode_number"] is None:
+            videos.pop(index)
+
+    return videos
+
+
+
+def get_sorted_videos(playlist_id):
+        videos = search_videos_from_playlist(playlist_id)
+
+        sort_videos(videos)
+
+        for video in videos:
+            print(f"Title: {video['title']}\nEpisode Number: {video['episode_number']}\nLink: {video['url']}")
+            print()
+
+        print(f"Videos Retrieved: {len(videos)}")
+
+        return videos
+
+
+def sort_videos(videos):
+    videos.sort(key=lambda vid:vid["episode_number"])
